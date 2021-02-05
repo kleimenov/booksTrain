107f397fdb42251a0901d54120bdff4fdd83cd08
Иван <kleimenov416@gmail.com>
2021-02-05 15:39:14 -0500

    implement login/logout logic

--- src/components/HeaderComponents/Logout.js
+++ src/components/HeaderComponents/Logout.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Redirect, Link } from "react-router-dom";
 
 const Logout = () => {
-    const logout = () => {
-        localStorage.clear();
-        window.location.href = '/';
-    }
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
   return (
     <div>
       <Link to="/">
-        <button onClick={ logout } className="btn signIn-submit-button">Log Out</button>
+        <button onClick={logout} className="btn signIn-submit-button">
+          Log Out
+        </button>
       </Link>
     </div>
   );
