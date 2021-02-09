f4daf202983a0eb759ab4de28c0682ede2992099
Иван <kleimenov416@gmail.com>
2021-02-09 10:40:36 -0500

    add assets folder, inside this folder add fonts and images folders

--- src/components/MainComponents/Homepage.js
+++ src/components/MainComponents/Homepage.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 const Homepage = (props) => {
-  if(!props.token) {
-    return(
-      <div className="container">
-      <h3 className="text-secondary">
-        let's find right book for you!
-      </h3>
-    </div>
-    )
+  if (!props.token) {
+    return (
+      <div className="homepage-background-img">
+        <div className="container">
+          <h3 className="text-secondary">let's find right book for you!</h3>
+        </div>
+      </div>
+    );
   }
   return (
-    <div className="container">
-      <h3 className="text-secondary">
-        Welcome {props.userName}, let's find right book for you!
-      </h3>
+    <div className="homepage-background-img">
+      <div className="container">
+        <h3 className="text-secondary">
+          Welcome {props.userName}, let's find right book for you!
+        </h3>
+      </div>
     </div>
   );
 };
