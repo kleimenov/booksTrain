00030ac9a9f0229a7261d4132cacae4c6006f56f
Иван <kleimenov416@gmail.com>
2021-02-09 12:15:57 -0500

    start working at home page, implement UX before signin

--- src/components/MainComponents/Homepage.js
+++ src/components/MainComponents/Homepage.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
+import axios from 'axios';
 
 const Homepage = (props) => {
+
+  const [booksList, setBooksList] =useState([])
+  const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699&key=AIzaSyAsjgXclc-8h_dCPaSFIler-zx8pzKPaFY";
+
+  useEffect(() => {
+    getBooksList();
+  }, []);
+  
+  const getBooksList =  async () => {
+    axios.request(url).then((response) => {
+      //console.log(response.data.items[1].volumeInfo.title)
+      setBooksList(response.data);
+    }).catch((error) => {
+      console.error(error);
+    })
+  };
+  
+  booksList.map((item )=> {
+    console.log(item)
+  })
+  
+
   if (!props.token) {
     return (
         <div className="container">
           <h3 className="text-secondary">let's find right book for you!</h3>
-          
         </div>
     );
   }
