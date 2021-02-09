dfa155441a248d694e9fbb4f56796b87d0ec06e3
Иван <kleimenov416@gmail.com>
2021-02-09 15:54:17 -0500

    start working at home page, implement UX before signin

--- src/components/MainComponents/Books.js
+++ src/components/MainComponents/Books.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Books = (props) => {
-    return (
-        <ul>
-            <img src={props.image} alt=""/>
-            <h4>Title: {props.title} </h4>
-            <span>{props.author}</span>
-            <p>Published: {props.desc}</p>
-        </ul>
-    )
-}
+  return (
+    <li className="books-list-wrapper">
+      <img src={props.image} alt="" className="book-img"/>
+      <div className="book-desc">
+        <h4>Title: {props.title} </h4>
+        <span>{props.author}</span>
+        <p>Published: {props.desc}</p>
+      </div>
+    </li>
+  );
+};
 
 export default Books;
