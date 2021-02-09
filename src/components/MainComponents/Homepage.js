5358178013798cd3c31f982085109d584173cf32
Иван <kleimenov416@gmail.com>
2021-02-09 11:09:20 -0500

    start working at home page, implement UX before signin

--- src/components/MainComponents/Homepage.js
+++ src/components/MainComponents/Homepage.js
@@ -3,21 +3,18 @@ import React from "react";
 const Homepage = (props) => {
   if (!props.token) {
     return (
-      <div className="homepage-background-img">
         <div className="container">
           <h3 className="text-secondary">let's find right book for you!</h3>
+          
         </div>
-      </div>
     );
   }
   return (
-    <div className="homepage-background-img">
       <div className="container">
         <h3 className="text-secondary">
           Welcome {props.userName}, let's find right book for you!
         </h3>
       </div>
-    </div>
   );
 };
 
