2f0af0b7d4239533dbc3e2769d73d3bf77761b2b
Иван <kleimenov416@gmail.com>
2021-02-10 12:24:53 -0500

    remove some code that I don't use

--- src/components/MainComponents/Homepage.js
+++ src/components/MainComponents/Homepage.js
@@ -35,25 +35,6 @@ const Homepage = (props) => {
  */
 
  
-
-
-/*
-let slideIndex = 0;
-showSlides();
-
-
-function showSlides() {
-  
-  let slides = document.getElementsByClassName("books-list-wrapper");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 4000); // Change image every 3 seconds
-}
-*/
   if (!props.token) {
     return (
       <div className="container">
