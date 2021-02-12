5addc7204cbc333996ef7923c0e5d56e08c8005d
Иван <kleimenov416@gmail.com>
2021-02-12 11:55:02 -0500

    add crate new table iside database and add data into table

--- src/components/MainComponents/Searchengine.js
+++ src/components/MainComponents/Searchengine.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Form, Card, Button } from "react-bootstrap";
 
 const Searchengine = () => {
+
+  
   return (
     <Card className="text-left">
       <Card.Body>
         <Form className="text-secondary search-form-settings">
           <Form.Group id="Author">
             <Form.Label>Search by Author</Form.Label>
-            <Form.Control type="Author" required />
+            <Form.Control type="Author" />
           </Form.Group>
           <Form.Group id="Genre">
             <Form.Label>Search by Genre</Form.Label>
-            <Form.Control type="Genre" required />
+            <Form.Control type="Genre" />
           </Form.Group>
           <Form.Group id="Country">
             <Form.Label>Search by Country</Form.Label>
-            <Form.Control type="Country" required />
+            <Form.Control type="Country" />
           </Form.Group>
           <Button
             className="w-50 btn-secondary search-button-settings"
