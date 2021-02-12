6e4e9cee0c5ea861b85f22c61b2b557a96d54e14
Иван <kleimenov416@gmail.com>
2021-02-12 12:42:54 -0500

    implement function checkEmptyInputs, function check is ther empty input or not

--- src/components/MainComponents/Searchengine.js
+++ src/components/MainComponents/Searchengine.js
@@ -2,40 +2,64 @@ import React, { useState } from "react";
 import { Form, Card, Button } from "react-bootstrap";
 
 const Searchengine = () => {
-
   const [Author, setAuthor] = useState();
-  const [Genre, setGenre ] = useState();
-  const [Country, setCountry ] = useState();
+  const [Genre, setGenre] = useState();
+  const [Country, setCountry] = useState();
 
   const data = {
     searchAuthor: Author,
     searchGenre: Genre,
-    searchCountry: Country
-  }
+    searchCountry: Country,
+  };
+  const checkEmptyInputs = (data, passPermit) => {
+    let cnt = 0;
+    for (let item in data) {
+      if (!data[item] || data[item] == "") {
+        cnt++;
+      }
+    }
+    cnt > 2 ? (passPermit = false) : (passPermit = true);
+  };
+
   const searchHandler = (evt) => {
+    const requestPermit = false;
 
-    evt.preventDefault()
+    evt.preventDefault();
 
-    console.log(data.length)
-  }
+    //if(Object.values(data)==undefined){console.log('Xyi')}else{console.log('Ne Xyi')}
+
+    checkEmptyInputs(data, requestPermit);
+  };
 
   return (
     <Card className="text-left">
       <Card.Body>
-        <Form className="text-secondary search-form-settings" onSubmit={ searchHandler }>
+        <Form
+          className="text-secondary search-form-settings"
+          onSubmit={searchHandler}
+        >
           <Form.Group id="Author">
             <Form.Label>Search by Author</Form.Label>
-            <Form.Control type="Author" onChange={(evt) => setAuthor(evt.target.value)}/>
+            <Form.Control
+              type="Author"
+              onChange={(evt) => setAuthor(evt.target.value)}
+            />
           </Form.Group>
           <Form.Group id="Genre">
             <Form.Label>Search by Genre</Form.Label>
-            <Form.Control type="Genre" onChange={(evt) => setGenre(evt.target.value)}/>
+            <Form.Control
+              type="Genre"
+              onChange={(evt) => setGenre(evt.target.value)}
+            />
           </Form.Group>
           <Form.Group id="Country">
             <Form.Label>Search by Country</Form.Label>
-            <Form.Control type="Country" onChange={(evt) => setCountry(evt.target.value)}/>
+            <Form.Control
+              type="Country"
+              onChange={(evt) => setCountry(evt.target.value)}
+            />
           </Form.Group>
-          <Button 
+          <Button
             className="w-50 btn-secondary search-button-settings"
             type="submit"
           >
