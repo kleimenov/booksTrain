65c2472999937c6a0c71fbe6a7618ef6a3660ee4
Иван <kleimenov416@gmail.com>
2021-02-14 16:53:34 -0500

    make code lines clear

--- src/components/MainComponents/Searchengine.js
+++ src/components/MainComponents/Searchengine.js
@@ -46,7 +46,7 @@ const Searchengine = () => {
       console.log("Xyi");
     }
   };
-  console.log(searchResponse);
+  //console.log(searchResponse);
   return (
     <Card className="text-left">
       <Card.Body>
