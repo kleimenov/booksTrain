ec6bffc854df908cf0da875b09a0034dc36431a0
Иван <kleimenov416@gmail.com>
2021-02-16 14:30:29 -0500

    start working at Searchresultitem component style

--- src/components/MainComponents/Searchresultitem.js
+++ src/components/MainComponents/Searchresultitem.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-const Searchresultitem =({ bookName, bookAuthor })=>{
-
-    return (
-    <li>
-       <h5 className="text-secondary">{ bookName } / { bookAuthor } </h5> 
+const Searchresultitem = ({ bookName, bookAuthor }) => {
+  return (
+    <li className="my-2">
+      <div className="d-flex flex-row">
+        <Button className="btn-success p-0 px-1" type="submit">
+          +book
+        </Button>
+        <h5 className="text-secondary ml-2 pt-2">
+          {bookName} / {bookAuthor}{" "}
+        </h5>
+      </div>
     </li>
-    )
-}
+  );
+};
 
 export default Searchresultitem;
