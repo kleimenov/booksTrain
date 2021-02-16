8319128277701859f1e03f6d219b0bec8d172257
Иван <kleimenov416@gmail.com>
2021-02-16 16:03:59 -0500

    implement myBooksFetch function, I will use this function fetching data from database

--- src/components/MainComponents/Mybooks.js
+++ src/components/MainComponents/Mybooks.js
@@ -1,9 +1,20 @@
+import { json } from "body-parser";
 import React from "react";
 
+const myBooksFetch = async (credentials) => {
+    return fetch("http://localhost:3002/booksSearch", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      }, 
+      body: json.stringify(credentials),
+    }).then((response) => response.json());
+  };
 
-const Mybooks = ( { myBooks, setMyBooks }) => {
+const Mybooks = ({ myBooks, setMyBooks }) => {
 
-  
+
+
   return (
     <div>
       <h3 className="text-secondary">This is Mybooks books page</h3>
