8819bc058b221ae491420b47e9b2e7e8ba0335dd
Иван <kleimenov416@gmail.com>
2021-02-16 15:58:22 -0500

    start working at myBooks component

--- src/components/MainComponents/Mybooks.js
+++ src/components/MainComponents/Mybooks.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Mybooks = () => {
+
+const Mybooks = ( { myBooks, setMyBooks }) => {
+
+  
   return (
     <div>
       <h3 className="text-secondary">This is Mybooks books page</h3>
