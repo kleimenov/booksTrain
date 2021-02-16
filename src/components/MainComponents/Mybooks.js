57766d846b4c3cb1421a1a46b4d2b669f0893397
Иван <kleimenov416@gmail.com>
2021-02-16 16:45:36 -0500

    implement uplifting state for userData and particular user list of books

--- src/components/MainComponents/Mybooks.js
+++ src/components/MainComponents/Mybooks.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 const myBooksFetch = async (credentials) => {
-    return fetch("http://localhost:3002/userBooks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      }, 
-      body: JSON.stringify(credentials),
-    }).then((response) => response.json());
-  };
+  return fetch("http://localhost:3002/userBooks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((response) => response.json());
+};
 
 const Mybooks = ({ myBooks, setMyBooks, userData }) => {
-
-  console.log( userData )
-  console.log('Xyi')
+  console.log(userData);
 
   
 
