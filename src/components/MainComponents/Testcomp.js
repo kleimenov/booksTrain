0f1512633def534283d79889c6aa320831abe59e
Иван <kleimenov416@gmail.com>
2021-02-17 17:36:41 -0500

    clear code

--- src/components/MainComponents/Testcomp.js
+++ src/components/MainComponents/Testcomp.js
@@ -1,6 +1,4 @@
-import React from 'react'
+import React from "react";
 
-const Welcome = (props) => (
-     <h1>Hello, {props.title} John</h1>
-)
-export default Welcome
+const Welcome = (props) => <h1>Hello, {props.title} John</h1>;
+export default Welcome;
