f6f28cdaf1ee5c52e80342aba3a8ec000d5602bd
Иван <kleimenov416@gmail.com>
2021-02-17 10:36:39 -0500

    start working at convert unix time to current time

--- src/components/MainComponents/Userbook.js
+++ src/components/MainComponents/Userbook.js
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Userbook = ({ bookName, bookAuthor, date }) => {
 
- let unixTime = parseInt(date)
+  const [ textColor, setColor ] = useState()
+
+  //let unixTime = parseInt(date);
+  //const currentData = new Date().getTime();
+  //console.log("Unix current time " +currentData)
+  //let uTime = new Date(unixTime);
+  //console.log("Human radable time from server" +uTime)
+
+  //let differenceTime = currentData - unixTime;
+  //let value = Math.floor(differenceTime / (1000 * 60 * 60) / 24);
 
 
 
+  const countdounTime = (date) => {
+    const readingTerm = 14;
+    const startUnixTime = parseInt(date);
+    const currentuUixTime = new Date().getTime();
+    const unixDiff = currentuUixTime - startUnixTime;
 
- const toCapitalize = (string) => {
-  let splitStr = string.split(' ');
-  let newArr = [];
-  for (let word of splitStr) {
-    if(word.length > 1) {
-      word = word.charAt(0).toUpperCase() + word.slice(1);
-      newArr.push(word)
-    } else {newArr.push(word)}
-  }
-  return newArr.join(' ');
-}
+    return readingTerm - Math.floor(unixDiff / (1000 * 60 * 60) / 24);
+  };
+
+  const toCapitalize = (string) => {
+    let splitStr = string.split(" ");
+    let newArr = [];
+    for (let word of splitStr) {
+      if (word.length > 1) {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        newArr.push(word);
+      } else {
+        newArr.push(word);
+      }
+    }
+    return newArr.join(" ");
+  };
 
   return (
     <li className="my-2">
-        <h5 className="text-secondary pt-2">
-          {toCapitalize(bookName)} / {toCapitalize(bookAuthor) } / { unixTime }
-        </h5>
+      <h5 className="text-secondary pt-2">
+        {toCapitalize(bookName)} / {toCapitalize(bookAuthor)} /{" "}
+        <h6 className={countdounTime(date) > 5 ? }>
+          {countdounTime(date) > 1
+            ? `You have ${countdounTime(
+                date
+              )} days till the end of reading term`
+            : `You have ${countdounTime(date)} one day the end of reading term`}
+        </h6>
+      </h5>
     </li>
   );
 };
