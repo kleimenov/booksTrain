58583dd846e308751600abc5a6890e630dfc7ce2
Иван <kleimenov416@gmail.com>
2021-02-17 17:36:24 -0500

    clear code

--- src/components/MainComponents/Searchresult.js
+++ src/components/MainComponents/Searchresult.js
@@ -17,21 +17,21 @@ const Searchresult = ({ searchResult, setResponse }) => {
 
   const capitalizaResult = (data) => {
     const titleCase = (string) => {
-      let splitStr = string.split(' ');
+      let splitStr = string.split(" ");
       let newArr = [];
       for (let word of splitStr) {
         word = word.charAt(0).toUpperCase() + word.slice(1);
-        newArr.push(word)
+        newArr.push(word);
       }
-      return newArr.join(' ');
-    }
+      return newArr.join(" ");
+    };
 
     data.map((item) => {
       let tmp = item.book_name.split(" ");
       tmp[0] = tmp[0][0].toUpperCase() + tmp[0].slice(1);
       item.book_name = tmp.join(" ");
 
-      item.author = titleCase(item.author)
+      item.author = titleCase(item.author);
     });
   };
 
