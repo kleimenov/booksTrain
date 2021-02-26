c779a3fd5ec423699af7b89ddca8daea38d99353
Иван <kleimenov416@gmail.com>
2021-02-26 17:16:26 -0500

    clean code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
-  useHistory,
+  Switch
 } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 //import components
