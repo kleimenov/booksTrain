a95827f5df656bb07f1adbc792282519fcb44146
Иван <kleimenov416@gmail.com>
2021-02-26 09:27:25 -0500

    working at return book feature

--- src/components/MainComponents/Returnbook.js
+++ src/components/MainComponents/Returnbook.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 const Returnbook = ({ leftTime, userId, bookId }) => {
-
-  console.log(bookId)
-  console.log(userId)
+  console.log(bookId);
+  console.log(userId);
 
   let width = 100 - Math.floor((leftTime / 14) * 100);
   const buttonStyles = {
     backgroundColor: `${width}` < 50 ? "#46b946" : "#f52a2a",
   };
-
 
   const returnBook = async (credentils) => {
     const response = await fetch("http://localhost:3002/returnbook", {
@@ -18,20 +16,24 @@ const Returnbook = ({ leftTime, userId, bookId }) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(credentils),
-    })
+    });
     const data = await response.json();
-  }
+  };
 
   const clickHandler = () => {
     returnBook({
       userId: userId,
-      bookId: bookId
-    })
-  }
+      bookId: bookId,
+    });
+  };
 
   return (
     <div>
-      <button onClick={clickHandler} className="btn return-book-button ml-2 px-1 py-0 text-white font-weight-bold" style={buttonStyles}>
+      <button
+        onClick={clickHandler}
+        className="btn return-book-button ml-2 px-1 py-0 text-white font-weight-bold"
+        style={buttonStyles}
+      >
         Return book
       </button>
     </div>
