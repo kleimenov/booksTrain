a7ff00739352178173972c558d75cd9182aa5ced
Иван <kleimenov416@gmail.com>
2021-02-26 08:59:25 -0500

    add new component and start working at return book logic

--- src/components/MainComponents/Returnbook.js
+++ src/components/MainComponents/Returnbook.js
@@ -7,9 +7,29 @@ const Returnbook = ({ leftTime }) => {
     backgroundColor: `${width}` < 50 ? "#46b946" : "#f52a2a",
   };
 
+
+  const returnBook = async (credentils) => {
+    const response = await fetch("http//localhost:3002/returnbook", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentils),
+    })
+    const data = await response.json();
+  }
+
+  const clickHandler = () => {
+    returnBook({
+      userId: userId,
+      bookId: bookId
+
+    })
+  }
+
   return (
     <div>
-      <button className="btn return-book-button ml-2 px-1 py-0 text-white font-weight-bold" style={buttonStyles}>
+      <button onClisk={clickHandler} className="btn return-book-button ml-2 px-1 py-0 text-white font-weight-bold" style={buttonStyles}>
         Return book
       </button>
     </div>
