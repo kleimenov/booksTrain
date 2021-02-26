bd9583f0bec844aa2e0374792c83000339fdb01a
Иван <kleimenov416@gmail.com>
2021-02-26 17:11:02 -0500

    clean code

--- src/components/MainComponents/Clock.js
+++ src/components/MainComponents/Clock.js
@@ -1,38 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-/*
-class Clock extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {date: new Date()};
-    }
-  
-    componentDidMount() {
-      this.timerID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
-  
-    componentWillUnmount() {
-      clearInterval(this.timerID);
-    }
-  
-    tick() {
-      this.setState({
-        date: new Date()
-      });
-    }
-  
-    render() {
-      return (
-        <div className="text-center text-secondary pt-1">
-          <h5>{this.state.date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</h5>
-        </div>
-      );
-    }
-  }
-*/
+
 
 const Clock = () => {
   const [currentTime, setCurrentTime] = useState(
