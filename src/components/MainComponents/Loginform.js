d22db3fd40289fd016d0e93a7221114db41a5832
Иван <kleimenov416@gmail.com>
2021-03-01 08:42:13 -0500

    remove unnecessary lines of code

--- src/components/MainComponents/Loginform.js
+++ src/components/MainComponents/Loginform.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 async function loginUser(credentials) {
   return fetch("http://localhost:3002/login", {
