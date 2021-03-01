c1c6880f66789e476d6d9e97b8a25fb69681d17a
Иван <kleimenov416@gmail.com>
2021-03-01 08:41:51 -0500

    remove unnecessary lines of code

--- src/components/MainComponents/Loginform.js
+++ src/components/MainComponents/Loginform.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { Redirect, useHistory } from "react-router-dom";
 
 async function loginUser(credentials) {
   return fetch("http://localhost:3002/login", {
