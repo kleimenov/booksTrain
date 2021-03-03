1c5fc7348ae9aec4aee1179f836bec6f9def7bd7
Иван <kleimenov416@gmail.com>
2021-03-03 12:48:57 -0500

    remove unnecessary lines of code

--- src/components/MainComponents/Wishbook.js
+++ src/components/MainComponents/Wishbook.js
@@ -3,7 +3,7 @@ import Removefromwishlist from "./Removefromwishlist";
 import { toCapitalize } from "./Handlers";
 
 const Wishbook = ({ bookName, bookAuthor, bookId, userId, getWishlist }) => {
-  console.log(bookName);
+
   return (
     <li>
       <div className="d-flex flex-row justify-content-between w-50">
